6d7078350757af3e39fbd11856f25e9e487bacd6
saifullah3396 <saifullah3396@gmail.com>
2024-09-16 13:39:09 +0200

    update tests.

--- scripts/test_metrics.sh
+++ scripts/test_metrics.sh
@@ -2,19 +2,18 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 export PYTHONPATH=$SCRIPTPATH/../src:$SCRIPTPATH/../tests
 
-# axiomatic done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/axiomatic/test_completeness.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/axiomatic/test_non_sensitivity.py # done
+# axiomatic
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/axiomatic/test_completeness.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/axiomatic/test_non_sensitivity.py
 
-# complexity done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_complexity.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_effective_complexity.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_sparseness.py # done
+# complexity
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_complexity.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_effective_complexity.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/complexity/test_sparseness.py
 
-# faithfulness done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_monotonicity_corr.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_monotonicity.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_faithfulness_corr.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_faithfulness_estimate.py # done
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_infidelity.py # already done in captum
-python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_aopc.py # already done in captum
+# # faithfulness
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_monotonicity_corr.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_monotonicity.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_faithfulness_corr.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_faithfulness_estimate.py
+python -W ignore -m unittest -v $SCRIPTPATH/../tests/metrics/faithfulness/test_aopc.py

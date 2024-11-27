5edbeeb5f7c5905292f7c02f803ffb28364f733e
Saifullah <saifullah.saifullah@dfki.de>
2024-11-27 14:02:06 +0100

    Refactor compute_abpc_scores function to improve tensor handling and add a new wrapper function for compatibility with numpy inputs.

--- src/torchxai/metrics/faithfulness/abpc.py
+++ src/torchxai/metrics/faithfulness/abpc.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 
-def compute_abpc_scores(aopc_scores: torch.Tensor) -> torch.Tensor:
+def compute_abpc_scores_tensor(aopc_scores: torch.Tensor) -> torch.Tensor:
     """
     Computes the ABPC score for the given AOPC scores over the complete dataset.
 
@@ -15,23 +15,26 @@ def compute_abpc_scores(aopc_scores: torch.Tensor) -> torch.Tensor:
             order of feature importance, the second row corresponds to the ascending order of feature importance and
             the rest of the rows correspond to the random order of feature importance.
     """
+    with torch.no_grad():
+        from scipy.integrate import simps
 
-    from scipy.integrate import simps
+        n_steps = aopc_scores.shape[2]
+        morf = aopc_scores[:, 0, :].numpy().mean(
+            0
+        )  # first row is descending, take mean over the dataset
+        lerf = aopc_scores[:, 1, :].numpy().mean(
+            0
+        )  # second row is ascending, take mean over the dataset
+        
+        morf_mean = simps(
+            morf, np.arange(0, n_steps)
+        )  # find the area under the curve for descending order
+        lerf_mean = simps(
+            lerf, np.arange(0, n_steps)
+        )  # find the area under the curve for ascending order
 
-    n_steps = aopc_scores.shape[1]
-    morf = np.array(aopc_scores[:, 0, :]).mean(
-        0
-    )  # first row is descending, take mean over the dataset
-    lerf = np.array(aopc_scores[:, 1, :]).mean(
-        0
-    )  # second row is ascending, take mean over the dataset
+        abpc_score = morf_mean - lerf_mean
+        return abpc_score
 
-    morf_mean = simps(
-        morf, np.arange(0, n_steps)
-    )  # find the area under the curve for descending order
-    lerf_mean = simps(
-        lerf, np.arange(0, n_steps)
-    )  # find the area under the curve for ascending order
-
-    abpc_score = morf_mean - lerf_mean
-    return abpc_score
+def compute_abpc_scores(morf_scores: torch.Tensor, lerf_scores: torch.Tensor) -> torch.Tensor:
+    return compute_abpc_scores_tensor(torch.stack([torch.from_numpy(morf_scores), torch.from_numpy(lerf_scores)], dim=1))

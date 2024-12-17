Fixed the logic of multi-target metric computation after updates.

src/torchxai/metrics/axiomatic/input_invariance.py
@@ -7,10 +7,103 @@ from captum.attr import Attribution
 from torch import Tensor
 
 from torchxai.explainers.explainer import Explainer
+from torchxai.metrics.axiomatic.multi_target.input_invariance import (
+    _multi_target_input_invariance,
+)
 from torchxai.metrics.axiomatic.utilities import (
     _create_shifted_expainer,
     _prepare_kwargs_for_base_and_shifted_inputs,
 )
+
+
+def _input_invariance(
+    explainer: Union[Explainer, Attribution],
+    inputs: TensorOrTupleOfTensorsGeneric,
+    constant_shifts: TensorOrTupleOfTensorsGeneric,
+    input_layer_names: Tuple[str],
+    **kwargs: Any,
+) -> Union[Tensor, List[Tensor]]:
+
+    kwargs_copy, shifted_kwargs_copy = _prepare_kwargs_for_base_and_shifted_inputs(
+        kwargs
+    )
+    inputs = _format_tensor_into_tuples(inputs)  # type: ignore
+    constant_shifts = _format_tensor_into_tuples(constant_shifts)  # type: ignore
+
+    assert len(input_layer_names) == len(
+        set(input_layer_names)
+    ), "Each input layer must be unique for each input constant shift tensor."
+
+    assert len(input_layer_names) == len(
+        constant_shifts
+    ), "The number of input layer names should be the same as the number of constant shifts. "
+
+    assert (
+        len(inputs) == len(constant_shifts)
+        and inputs[0].shape[1:] == constant_shifts[0].shape[1:]
+        and constant_shifts[0].shape[0] == 1
+    ), (
+        "The number of inputs should be the same as the number of constant shifts and the batch size of the "
+        "constant shifts should be 1. Current shapes are: "
+        f"{inputs[0].shape} and {constant_shifts[0].shape}"
+    )
+
+    shifted_explainer = _create_shifted_expainer(
+        explainer=explainer,
+        input_layer_names=input_layer_names,
+        constant_shifts=constant_shifts,
+        **kwargs,
+    )
+
+    # create shifted inputs
+    constant_shift_expanded = tuple(
+        constant_shift.expand_as(input)
+        for input, constant_shift in zip(inputs, constant_shifts)
+    )
+    shifted_inputs = tuple(
+        input - constant_shift
+        for input, constant_shift in zip(inputs, constant_shift_expanded)
+    )
+
+    with torch.no_grad():
+        if isinstance(explainer, Explainer):
+            inputs_expl = explainer.explain(inputs, **kwargs_copy)
+            shifted_inputs_expl = shifted_explainer.explain(
+                shifted_inputs, **shifted_kwargs_copy
+            )
+        elif isinstance(explainer, Attribution):
+            inputs_expl = explainer.attribute(inputs, **kwargs_copy)
+            shifted_inputs_expl = shifted_explainer.attribute(
+                shifted_inputs, **shifted_kwargs_copy
+            )
+        else:
+            raise ValueError(
+                "Explanation function must be an instance of Attribution or FusionExplainer"
+            )
+
+        # calculate the difference between the two explanations
+        input_invarance_score = sum(
+            tuple(
+                torch.tensor(
+                    [
+                        torch.mean(
+                            torch.abs(
+                                per_sample_input_expl - per_sample_shifted_input_expl
+                            )
+                        ).item()
+                        for per_sample_input_expl, per_sample_shifted_input_expl in zip(
+                            input_expl, shifted_input_expl
+                        )
+                    ],
+                    device=inputs[0].device,
+                )
+                for input_expl, shifted_input_expl in zip(
+                    inputs_expl, shifted_inputs_expl
+                )
+            )
+        )
+
+        return input_invarance_score, inputs_expl, shifted_inputs_expl
 
 
 def input_invariance(
@@ -114,135 +207,46 @@ def input_invariance(
         >>> input_invarance_score, inputs_expl, shifted_inputs_expl = input_invarance(saliency, input, target = 3)
 
     """
-    if is_multi_target:
-        from torchxai.metrics.axiomatic.multi_target.input_invariance import (
-            _multi_target_input_invariance,
-        )
-
-        input_invarance_score, inputs_expl, shifted_inputs_expl = (
-            _multi_target_input_invariance(
-                explainer=explainer,
-                inputs=inputs,
-                constant_shifts=constant_shifts,
-                input_layer_names=input_layer_names,
-                **kwargs,
-            )
-        )
-        if return_intermediate_results:
-            if return_dict:
-                return {
-                    "input_invarance_score": input_invarance_score,
-                    "inputs_expl": inputs_expl,
-                    "shifted_inputs_expl": shifted_inputs_expl,
-                }
-            else:
-                return (input_invarance_score, inputs_expl, shifted_inputs_expl)
-        else:
-            if return_dict:
-                return {"input_invarance_score": input_invarance_score}
-            else:
-                return input_invarance_score
+    metric_func = (
+        _multi_target_input_invariance if is_multi_target else _input_invariance
+    )
+    input_invarance_score, inputs_expl, shifted_inputs_expl = metric_func(
+        explainer=explainer,
+        inputs=inputs,
+        constant_shifts=constant_shifts,
+        input_layer_names=input_layer_names,
+        **kwargs,
+    )
 
     # Keeps track whether original input is a tuple or not before
     # converting it into a tuple.
     is_inputs_tuple = _is_tuple(inputs)
 
-    kwargs_copy, shifted_kwargs_copy = _prepare_kwargs_for_base_and_shifted_inputs(
-        kwargs
-    )
-    inputs = _format_tensor_into_tuples(inputs)  # type: ignore
-    constant_shifts = _format_tensor_into_tuples(constant_shifts)  # type: ignore
-
-    assert len(input_layer_names) == len(
-        set(input_layer_names)
-    ), "Each input layer must be unique for each input constant shift tensor."
-
-    assert len(input_layer_names) == len(
-        constant_shifts
-    ), "The number of input layer names should be the same as the number of constant shifts. "
-
-    assert (
-        len(inputs) == len(constant_shifts)
-        and inputs[0].shape[1:] == constant_shifts[0].shape[1:]
-        and constant_shifts[0].shape[0] == 1
-    ), (
-        "The number of inputs should be the same as the number of constant shifts and the batch size of the "
-        "constant shifts should be 1. Current shapes are: "
-        f"{inputs[0].shape} and {constant_shifts[0].shape}"
-    )
-
-    shifted_explainer = _create_shifted_expainer(
-        explainer=explainer,
-        input_layer_names=input_layer_names,
-        constant_shifts=constant_shifts,
-        **kwargs,
-    )
-
-    # create shifted inputs
-    constant_shift_expanded = tuple(
-        constant_shift.expand_as(input)
-        for input, constant_shift in zip(inputs, constant_shifts)
-    )
-    shifted_inputs = tuple(
-        input - constant_shift
-        for input, constant_shift in zip(inputs, constant_shift_expanded)
-    )
-
-    with torch.no_grad():
-        if isinstance(explainer, Explainer):
-            inputs_expl = explainer.explain(inputs, **kwargs_copy)
-            shifted_inputs_expl = shifted_explainer.explain(
-                shifted_inputs, **shifted_kwargs_copy
-            )
-        elif isinstance(explainer, Attribution):
-            inputs_expl = explainer.attribute(inputs, **kwargs_copy)
-            shifted_inputs_expl = shifted_explainer.attribute(
-                shifted_inputs, **shifted_kwargs_copy
-            )
+    if return_intermediate_results:
+        if return_dict:
+            return {
+                "input_invarance_score": input_invarance_score,
+                "inputs_expl": _format_output(is_inputs_tuple, inputs_expl),
+                "shifted_inputs_expl": _format_output(
+                    is_inputs_tuple, shifted_inputs_expl
+                ),
+            }
         else:
-            raise ValueError(
-                "Explanation function must be an instance of Attribution or FusionExplainer"
+            return (
+                input_invarance_score,
+                (
+                    _format_output(is_inputs_tuple, inputs_expl)
+                    if not isinstance(inputs_expl, list)
+                    else inputs_expl
+                ),
+                (
+                    _format_output(is_inputs_tuple, shifted_inputs_expl)
+                    if not isinstance(shifted_inputs_expl, list)
+                    else shifted_inputs_expl
+                ),
             )
-
-        # calculate the difference between the two explanations
-        input_invarance_score = sum(
-            tuple(
-                torch.tensor(
-                    [
-                        torch.mean(
-                            torch.abs(
-                                per_sample_input_expl - per_sample_shifted_input_expl
-                            )
-                        ).item()
-                        for per_sample_input_expl, per_sample_shifted_input_expl in zip(
-                            input_expl, shifted_input_expl
-                        )
-                    ],
-                    device=inputs[0].device,
-                )
-                for input_expl, shifted_input_expl in zip(
-                    inputs_expl, shifted_inputs_expl
-                )
-            )
-        )
-
-        if return_intermediate_results:
-            if return_dict:
-                return {
-                    "input_invarance_score": input_invarance_score,
-                    "inputs_expl": _format_output(is_inputs_tuple, inputs_expl),
-                    "shifted_inputs_expl": _format_output(
-                        is_inputs_tuple, shifted_inputs_expl
-                    ),
-                }
-            else:
-                return (
-                    input_invarance_score,
-                    _format_output(is_inputs_tuple, inputs_expl),
-                    _format_output(is_inputs_tuple, shifted_inputs_expl),
-                )
+    else:
+        if return_dict:
+            return {"input_invarance_score": input_invarance_score}
         else:
-            if return_dict:
-                return {"input_invarance_score": input_invarance_score}
-            else:
-                return input_invarance_score
+            return input_invarance_score

src/torchxai/metrics/axiomatic/monotonicity_corr_and_non_sens.py
@@ -166,14 +166,10 @@ def eval_monotonicity_corr_and_non_sens_single_sample(
             current_n_steps,
             device=inputs[0].device,
         )
-        current_feature_indices = torch.arange(
-            current_n_steps - current_n_perturbed_features, current_n_steps
-        )
-        current_perturbation_mask = global_perturbation_masks[current_feature_indices]
         inputs_perturbed = _generate_perturbations(
-            current_n_perturbed_features, current_perturbation_mask
+            current_n_perturbed_features,
+            global_perturbation_masks[current_feature_indices],
         )
-        # _draw_perturbated_inputs_sequences_images(inputs_perturbed)
         inputs_perturbed = _format_tensor_into_tuples(inputs_perturbed)
         _validate_inputs_and_perturbations(
             cast(Tuple[Tensor, ...], inputs),
@@ -258,9 +254,9 @@ def eval_monotonicity_corr_and_non_sens_single_sample(
             # however this makes it impossible to perturb features together for multi-target scenario
             # overall the monotonicity_corr and non-sensitivity should not change too much for small beam sizes
             # and not at all for beam size 0 as the order of the features is not important in that case
-            ascending_attribution_indices = torch.argsort(reduced_attributions)
+            feature_indices = torch.argsort(reduced_attributions)
         else:
-            ascending_attribution_indices = torch.arange(
+            feature_indices = torch.arange(
                 reduced_attributions.size(0), device=inputs[0].device
             )
 
@@ -276,7 +272,7 @@ def eval_monotonicity_corr_and_non_sens_single_sample(
             _feature_mask_to_chunked_perturbation_mask_with_attributions(
                 feature_mask_flattened,
                 reduced_attributions,
-                ascending_attribution_indices,
+                feature_indices,
                 frozen_features,
                 percentage_feature_removal_per_step,
             )
@@ -391,6 +387,138 @@ def eval_monotonicity_corr_and_non_sens_single_sample(
         perturbed_fwd_diffs_relative_vars,
         chunk_reduced_attributions.cpu().numpy(),
     )
+
+
+def _monotonicity_corr_and_non_sens(
+    forward_func: Callable,
+    inputs: TensorOrTupleOfTensorsGeneric,
+    attributions: TensorOrTupleOfTensorsGeneric,
+    baselines: BaselineType = None,
+    feature_mask: TensorOrTupleOfTensorsGeneric = None,
+    additional_forward_args: Any = None,
+    target: TargetType = None,
+    frozen_features: Optional[List[torch.Tensor]] = None,
+    perturb_func: Callable = default_fixed_baseline_perturb_func(),
+    n_perturbations_per_feature: int = 10,
+    max_features_processed_per_batch: int = None,
+    percentage_feature_removal_per_step: float = 0.0,
+    zero_attribution_threshold: float = 1e-5,
+    zero_variance_threshold: float = 1e-5,
+    use_percentage_attribution_threshold: bool = False,
+    return_ratio: bool = True,
+    show_progress: bool = False,
+) -> Tuple[Tensor, Tensor, Tensor]:
+    with torch.no_grad():
+        # perform argument formattings
+        inputs = _format_tensor_into_tuples(inputs)  # type: ignore
+        baselines = _format_tensor_into_tuples(baselines)  # type: ignore
+        additional_forward_args = _format_additional_forward_args(
+            additional_forward_args
+        )
+        attributions = _format_tensor_into_tuples(attributions)  # type: ignore
+        feature_mask = _format_tensor_into_tuples(feature_mask)  # type: ignore
+
+        # Make sure that inputs and corresponding attributions have matching sizes.
+        assert len(inputs) == len(attributions), (
+            """The number of tensors in the inputs and
+            attributions must match. Found number of tensors in the inputs is: {} and in the
+            attributions: {}"""
+        ).format(len(inputs), len(attributions))
+        if feature_mask is not None:
+            for mask, attribution in zip(feature_mask, attributions):
+                assert mask.shape == attribution.shape, (
+                    """The shape of the feature mask and the attribution
+                    must match. Found feature mask shape: {} and attribution shape: {}"""
+                ).format(mask.shape, attribution.shape)
+
+        bsz = inputs[0].size(0)
+        monotonicity_corr_batch = []
+        non_sens_batch = []
+        n_features_batch = []
+        perturbed_fwd_diffs_relative_vars_batch = []
+        feature_group_attribution_scores_batch = []
+        for sample_idx in tqdm.tqdm(range(bsz), disable=not show_progress):
+            (
+                monotonicity_corr,
+                non_sens,
+                n_features,
+                perturbed_fwd_diffs_relative_vars,
+                feature_group_attribution_scores,
+            ) = eval_monotonicity_corr_and_non_sens_single_sample(
+                forward_func=forward_func,
+                inputs=tuple(input[sample_idx].unsqueeze(0) for input in inputs),
+                attributions=tuple(
+                    attr[sample_idx].unsqueeze(0) for attr in attributions
+                ),
+                baselines=(
+                    tuple(baseline[sample_idx].unsqueeze(0) for baseline in baselines)
+                    if baselines is not None
+                    else None
+                ),
+                feature_mask=(
+                    tuple(mask[sample_idx].unsqueeze(0) for mask in feature_mask)
+                    if feature_mask is not None
+                    else None
+                ),
+                additional_forward_args=(
+                    tuple(
+                        (
+                            arg[sample_idx].unsqueeze(0)
+                            if isinstance(arg, torch.Tensor)
+                            else arg
+                        )
+                        for arg in additional_forward_args
+                    )
+                    if additional_forward_args is not None
+                    else None
+                ),
+                target=(
+                    target[sample_idx]
+                    if isinstance(target, (list, torch.Tensor))
+                    else target
+                ),
+                frozen_features=(
+                    frozen_features[sample_idx]
+                    if frozen_features is not None
+                    else frozen_features
+                ),
+                perturb_func=perturb_func,
+                n_perturbations_per_feature=n_perturbations_per_feature,
+                max_features_processed_per_batch=max_features_processed_per_batch,
+                percentage_feature_removal_per_step=percentage_feature_removal_per_step,
+                zero_attribution_threshold=zero_attribution_threshold,
+                zero_variance_threshold=zero_variance_threshold,
+                use_percentage_attribution_threshold=use_percentage_attribution_threshold,
+                return_ratio=return_ratio,
+                show_progress=show_progress,
+            )
+
+            monotonicity_corr_batch.append(monotonicity_corr)
+            non_sens_batch.append(non_sens)
+            n_features_batch.append(n_features)
+            perturbed_fwd_diffs_relative_vars_batch.append(
+                perturbed_fwd_diffs_relative_vars
+            )
+            feature_group_attribution_scores_batch.append(
+                feature_group_attribution_scores
+            )
+        monotonicity_corr_batch = torch.tensor(monotonicity_corr_batch)
+        non_sens_batch = torch.tensor(non_sens_batch)
+        n_features_batch = torch.tensor(n_features_batch)
+        perturbed_fwd_diffs_relative_vars_batch = [
+            torch.tensor(x) for x in perturbed_fwd_diffs_relative_vars_batch
+        ]
+        feature_group_attribution_scores_batch = [
+            torch.tensor(x) for x in feature_group_attribution_scores_batch
+        ]
+
+        return (
+            monotonicity_corr_batch,
+            non_sens_batch,
+            n_features_batch,
+            perturbed_fwd_diffs_relative_vars_batch,
+            feature_group_attribution_scores_batch,
+        )
 
 
 def monotonicity_corr_and_non_sens(
@@ -684,185 +812,62 @@ def monotonicity_corr_and_non_sens(
         >>> # Computes the monotonicity correlation and non-sensitivity scores for saliency maps
         >>> monotonicity_corr, non_sens, n_features = monotonicity_corr_and_non_sens(net, input, attribution, baselines)
     """
-    if is_multi_target:
-        (
-            monotonicity_corr_batch_list,
-            non_sens_batch_list,
-            n_features_batch,
-            perturbed_fwd_diffs_relative_vars_batch_list,
-            feature_group_attribution_scores_batch_list,
-        ) = _multi_target_monotonicity_corr_and_non_sens(
-            forward_func=forward_func,
-            inputs=inputs,
-            attributions_list=attributions,
-            baselines=baselines,
-            feature_mask=feature_mask,
-            additional_forward_args=additional_forward_args,
-            targets_list=target,
-            frozen_features=frozen_features,
-            perturb_func=perturb_func,
-            n_perturbations_per_feature=n_perturbations_per_feature,
-            max_features_processed_per_batch=max_features_processed_per_batch,
-            percentage_feature_removal_per_step=percentage_feature_removal_per_step,
-            zero_attribution_threshold=zero_attribution_threshold,
-            zero_variance_threshold=zero_variance_threshold,
-            use_percentage_attribution_threshold=use_percentage_attribution_threshold,
-            return_ratio=return_ratio,
-            show_progress=show_progress,
-        )
-
-        if return_intermediate_results:
-            if return_dict:
-                return {
-                    "monotonicity_corr_score": monotonicity_corr_batch_list,
-                    "non_sensitivity_score": non_sens_batch_list,
-                    "n_features": n_features_batch,
-                    "perturbed_fwd_diffs_relative_vars": perturbed_fwd_diffs_relative_vars_batch_list,
-                    "feature_group_attribution_scores": feature_group_attribution_scores_batch_list,
-                }
-            else:
-                return (
-                    monotonicity_corr_batch_list,
-                    non_sens_batch_list,
-                    n_features_batch,
-                    perturbed_fwd_diffs_relative_vars_batch_list,
-                    feature_group_attribution_scores_batch_list,
-                )
+    metric_func = (
+        _multi_target_monotonicity_corr_and_non_sens
+        if is_multi_target
+        else _monotonicity_corr_and_non_sens
+    )
+    (
+        monotonicity_corr_batch,
+        non_sens_batch,
+        n_features_batch,
+        perturbed_fwd_diffs_relative_vars_batch,
+        feature_group_attribution_scores_batch,
+    ) = metric_func(
+        forward_func=forward_func,
+        inputs=inputs,
+        **(
+            dict(attributions_list=attributions)
+            if is_multi_target
+            else dict(attributions=attributions)
+        ),
+        baselines=baselines,
+        feature_mask=feature_mask,
+        additional_forward_args=additional_forward_args,
+        **(dict(targets_list=target) if is_multi_target else dict(target=target)),
+        frozen_features=frozen_features,
+        perturb_func=perturb_func,
+        n_perturbations_per_feature=n_perturbations_per_feature,
+        max_features_processed_per_batch=max_features_processed_per_batch,
+        percentage_feature_removal_per_step=percentage_feature_removal_per_step,
+        zero_attribution_threshold=zero_attribution_threshold,
+        zero_variance_threshold=zero_variance_threshold,
+        use_percentage_attribution_threshold=use_percentage_attribution_threshold,
+        return_ratio=return_ratio,
+        show_progress=show_progress,
+    )
+    if return_intermediate_results:
+        if return_dict:
+            return {
+                "monotonicity_corr_score": monotonicity_corr_batch,
+                "non_sensitivity_score": non_sens_batch,
+                "n_features": n_features_batch,
+                "perturbed_fwd_diffs_relative_vars": perturbed_fwd_diffs_relative_vars_batch,
+                "feature_group_attribution_scores": feature_group_attribution_scores_batch,
+            }
         else:
-            if return_dict:
-                return {
-                    "monotonicity_corr_score": monotonicity_corr_batch_list,
-                    "non_sensitivity_score": non_sens_batch_list,
-                }
-            else:
-                return monotonicity_corr_batch_list, non_sens_batch_list
-
-    with torch.no_grad():
-        # perform argument formattings
-        inputs = _format_tensor_into_tuples(inputs)  # type: ignore
-        baselines = _format_tensor_into_tuples(baselines)  # type: ignore
-        additional_forward_args = _format_additional_forward_args(
-            additional_forward_args
-        )
-        attributions = _format_tensor_into_tuples(attributions)  # type: ignore
-        feature_mask = _format_tensor_into_tuples(feature_mask)  # type: ignore
-
-        # Make sure that inputs and corresponding attributions have matching sizes.
-        assert len(inputs) == len(attributions), (
-            """The number of tensors in the inputs and
-            attributions must match. Found number of tensors in the inputs is: {} and in the
-            attributions: {}"""
-        ).format(len(inputs), len(attributions))
-        if feature_mask is not None:
-            for mask, attribution in zip(feature_mask, attributions):
-                assert mask.shape == attribution.shape, (
-                    """The shape of the feature mask and the attribution
-                    must match. Found feature mask shape: {} and attribution shape: {}"""
-                ).format(mask.shape, attribution.shape)
-
-        bsz = inputs[0].size(0)
-        monotonicity_corr_batch = []
-        non_sens_batch = []
-        n_features_batch = []
-        perturbed_fwd_diffs_relative_vars_batch = []
-        feature_group_attribution_scores_batch = []
-        for sample_idx in tqdm.tqdm(range(bsz), disable=not show_progress):
-            (
-                monotonicity_corr,
-                non_sens,
-                n_features,
-                perturbed_fwd_diffs_relative_vars,
-                feature_group_attribution_scores,
-            ) = eval_monotonicity_corr_and_non_sens_single_sample(
-                forward_func=forward_func,
-                inputs=tuple(input[sample_idx].unsqueeze(0) for input in inputs),
-                attributions=tuple(
-                    attr[sample_idx].unsqueeze(0) for attr in attributions
-                ),
-                baselines=(
-                    tuple(baseline[sample_idx].unsqueeze(0) for baseline in baselines)
-                    if baselines is not None
-                    else None
-                ),
-                feature_mask=(
-                    tuple(mask[sample_idx].unsqueeze(0) for mask in feature_mask)
-                    if feature_mask is not None
-                    else None
-                ),
-                additional_forward_args=(
-                    tuple(
-                        (
-                            arg[sample_idx].unsqueeze(0)
-                            if isinstance(arg, torch.Tensor)
-                            else arg
-                        )
-                        for arg in additional_forward_args
-                    )
-                    if additional_forward_args is not None
-                    else None
-                ),
-                target=(
-                    target[sample_idx]
-                    if isinstance(target, (list, torch.Tensor))
-                    else target
-                ),
-                frozen_features=(
-                    frozen_features[sample_idx]
-                    if frozen_features is not None
-                    else frozen_features
-                ),
-                perturb_func=perturb_func,
-                n_perturbations_per_feature=n_perturbations_per_feature,
-                max_features_processed_per_batch=max_features_processed_per_batch,
-                percentage_feature_removal_per_step=percentage_feature_removal_per_step,
-                zero_attribution_threshold=zero_attribution_threshold,
-                zero_variance_threshold=zero_variance_threshold,
-                use_percentage_attribution_threshold=use_percentage_attribution_threshold,
-                return_ratio=return_ratio,
-                show_progress=show_progress,
+            return (
+                monotonicity_corr_batch,
+                non_sens_batch,
+                n_features_batch,
+                perturbed_fwd_diffs_relative_vars_batch,
+                feature_group_attribution_scores_batch,
             )
-
-            monotonicity_corr_batch.append(monotonicity_corr)
-            non_sens_batch.append(non_sens)
-            n_features_batch.append(n_features)
-            perturbed_fwd_diffs_relative_vars_batch.append(
-                perturbed_fwd_diffs_relative_vars
-            )
-            feature_group_attribution_scores_batch.append(
-                feature_group_attribution_scores
-            )
-        monotonicity_corr_batch = torch.tensor(monotonicity_corr_batch)
-        non_sens_batch = torch.tensor(non_sens_batch)
-        n_features_batch = torch.tensor(n_features_batch)
-        perturbed_fwd_diffs_relative_vars_batch = [
-            torch.tensor(x) for x in perturbed_fwd_diffs_relative_vars_batch
-        ]
-        feature_group_attribution_scores_batch = [
-            torch.tensor(x) for x in feature_group_attribution_scores_batch
-        ]
-
-        if return_intermediate_results:
-            if return_dict:
-                return {
-                    "monotonicity_corr_score": monotonicity_corr_batch,
-                    "non_sensitivity_score": non_sens_batch,
-                    "n_features": n_features_batch,
-                    "perturbed_fwd_diffs_relative_vars": perturbed_fwd_diffs_relative_vars_batch,
-                    "feature_group_attribution_scores": feature_group_attribution_scores_batch,
-                }
-            else:
-                return (
-                    monotonicity_corr_batch,
-                    non_sens_batch,
-                    n_features_batch,
-                    perturbed_fwd_diffs_relative_vars_batch,
-                    feature_group_attribution_scores_batch,
-                )
+    else:
+        if return_dict:
+            return {
+                "monotonicity_corr_score": monotonicity_corr_batch,
+                "non_sensitivity_score": non_sens_batch,
+            }
         else:
-            if return_dict:
-                return {
-                    "monotonicity_corr_score": monotonicity_corr_batch,
-                    "non_sensitivity_score": non_sens_batch,
-                }
-            else:
-                return monotonicity_corr_batch, non_sens_batch
+            return monotonicity_corr_batch, non_sens_batch

src/torchxai/metrics/axiomatic/multi_target/completeness.py
@@ -8,6 +8,7 @@ from captum._utils.common import (
 )
 from captum._utils.typing import BaselineType, TargetType, TensorOrTupleOfTensorsGeneric
 from torch import Tensor
+
 from torchxai.explainers._utils import _run_forward_multi_target
 
 
@@ -15,7 +16,7 @@ def _multi_target_completeness(
     forward_func: Callable,
     inputs: TensorOrTupleOfTensorsGeneric,
     attributions_list: List[TensorOrTupleOfTensorsGeneric],
-    metric_baselines: BaselineType,
+    baselines: BaselineType,
     additional_forward_args: Any = None,
     targets_list: List[TargetType] = None,
 ) -> List[Tensor]:
@@ -34,12 +35,10 @@ def _multi_target_completeness(
         ).format(len(targets_list), len(attributions_list))
 
         inputs = _format_tensor_into_tuples(inputs)  # type: ignore
-        if metric_baselines is None:
-            metric_baselines = tuple(torch.zeros_like(inp) for inp in inputs)
+        if baselines is None:
+            baselines = tuple(torch.zeros_like(inp) for inp in inputs)
         else:
-            metric_baselines = _format_baseline(
-                metric_baselines, cast(Tuple[Tensor, ...], inputs)
-            )
+            baselines = _format_baseline(baselines, cast(Tuple[Tensor, ...], inputs))
         additional_forward_args = _format_additional_forward_args(
             additional_forward_args
         )
@@ -67,7 +66,7 @@ def _multi_target_completeness(
         # compute the forward pass on baselines
         baselines_fwd = _run_forward_multi_target(
             forward_func,
-            metric_baselines,
+            baselines,
             targets_list,
             additional_forward_args,
         )